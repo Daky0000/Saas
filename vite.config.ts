c85096a29da343e6fd63ddd02c2a59d4040ca306
Daky0000 <dakydaniilo@gmail.com>
2026-03-04 21:27:46 +0000

    Fix: Adjusted Vite base path for custom domain on GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Saas/', // Explicitly set base for GitHub Pages
+  base: './', // Adjusted base path for custom domain on GitHub Pages
   server: {
     port: 3000,
     proxy: {
